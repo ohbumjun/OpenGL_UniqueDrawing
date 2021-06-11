c836fa8ac541ae9ad1d50c44b33efd9f7760ee60
OH BUM JUN <beomjun0638@hanmail.net>
2021-06-11 14:06:52 +0900

    [Feat] Foreground.cpp background, sun pos change

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # OpenGL Simple Drawing App
+# a. beforeFinal
 
 ## 1. Description
 2d, 3d drawing application using openGL personal project 
@@ -86,7 +87,98 @@ ___
 ```sh
 download the code, 
 use the visual studio
-run the application in rootDir/연습
+run the application in rootDir/beforeFinal/연습
+```
+
+---
+# a. beforeFinal
+
+## 1. Description
+2d, 3d drawing application using openGL personal project 
+
+with the menu call back below, you can select
+which version of drawing you want to select 
+
+![](./readmeImg/menuselect1.png)
+![](./readmeImg/menuselect2.png)
+
+<br/>
+
+___ 
+
+# b. afterFinal
+## 1) simple drawing app
+![](./readmeImg/simpledraw1.png)
+### a. with keyboard(1,2,3,4,5,6,7) you can also choose figure
+
+<br/>
+
+![](./readmeImg/simpledraw2.png)
+
+### b. with keyboard('r','b','y','g' ...) you can select color
+
+![](./readmeImg/simpledraw3.png)
+
+<br/>
+
+### c. you can choose figure, and drag the position , also top figure will be chosen 
+
+<br/>
+<br/>
+
+___ 
+
+
+## 2) 2d clock 
+
+![](./readmeImg/2dclock.png)
+![](./readmeImg/2dclock2.png)
+
+
+### a. with keyboard('i','o') you can zoomin, zoom out
+### b. with keyboard('q') you can stop the animation of clock 
+### c. with keyboard('r','g' ...) you can change the color 
+
+<br/>
+<br/>
+
+___ 
+
+
+## 3) 3d robot 
+
+![](./readmeImg/3drobot.png)
+
+### a. camera is rotating around the 3d robot 
+
+![](./readmeImg/3drobot2.png)
+
+<br/>
+
+### b. with keyboard 'r,c,...' you can change the color of robot 
+
+<br/>
+
+![](./readmeImg/3drobot3.png)
+
+### c. you can choose 3 version (run,walk,chase)
+
+![](./readmeImg/3drobot4.png)
+
+### d. with key board 'f,s' you can fasten,slow the speed of robot movement
+
+<br/>
+<br/>
+
+___ 
+
+
+## Development setup
+
+```sh
+download the code, 
+use the visual studio
+run the application in rootDir/beforeFinal/연습
 ```
 
 ## Development Period
--- afterFinal/Project1/foreground.cpp
+++ afterFinal/Project1/foreground.cpp
@@ -5,8 +5,14 @@
 
 int w = 800;
 int h = 800;
-float backColor = 0;;
-
+float sunRotate = -10.0;
+float sunRotateDg = 0.005;
+struct backRGB {
+	float R = 0.6;
+	float G = 1.0;
+	float B = 1.0;
+	};
+backRGB backgrdRGB;
 using namespace std;
 
 void drawHouse()
@@ -25,15 +31,26 @@ void drawPerson() {
 }
 
 void drawSun() {
-	cout << "backColor :" << backColor << endl;
-	
+	float cx = 0.0, cy = 0.0, radius = 0.6;
+	float px = 0.0, pmx = 0.0;
+	float py = 0.0, pmy = 0.0;
+
 	glPushMatrix(); 
-		glRotatef(3.0 + backColor, 0.0, 0.0, 1.0);
-		line(vector3D(0, 0, 0), vector3D(-0.75, 0.5, 0));
+		glRotatef(sunRotate , 0.0, 0.0, 1.0);
 		glTranslatef(-0.75, 0.5, 0.0);
-		glRotatef(3.0 + backColor, 0.0, 0.0, 1.0);
-		glColor4f(0.5, 0.0, 0.0, 1);
+		glColor3f(1.0, 0.5, 0.0);
 		circle(vector3D(-0.0, 0.0, 0), 0.1);
+		for (float f = 0.0; f < 2 * M_PI; f += M_PI / 6) // 6.0 으로 나눈다는 것은 60씩 증가시킨다 --> 12개 그려진다 
+		{
+			glPushMatrix(); // --> 위에서 세팅된 x,y,z 에 기초하여 transformation이 이루어질 것이다
+				glRotatef(5*sunRotate, 0.0, 0.0, 1.0);
+				pmx = cx + (radius / 4) * cos(f);
+				pmy = cx + (radius / 4) * sin(f);
+				glTranslatef(pmx, pmy, 0.0);// instancing
+				glColor3f(1.0, 1.0, 0.0);
+				rect(vector3D(-0.01, 0.02, 0), vector3D(0.01, -0.02, 0));
+			glPopMatrix();
+		}
 	glPopMatrix();
 }
 
@@ -50,7 +67,8 @@ void drawTrees() {
 
 void drawBackground() {
 	// Background
-	glColor4f(0.6,1.0,1.0, 0.0);
+	glColor4f(backgrdRGB.R, backgrdRGB.G, backgrdRGB.B, 0.0); // -- 하늘색
+	// glColor4f(1.0, 0.8, 0.0, 0.0); -- 주황색 
 	rect(vector3D(0.9, -0.9, 0), vector3D(-0.9, 0.9, 0));
 	// Ground
 	glColor4f(0.4, 0.2, 0.1, 0.0);
@@ -75,7 +93,16 @@ void display()
 
 void idle()
 {
-	backColor += 0.0001;
+	
+	if (sunRotate > -100) {
+		sunRotate -= sunRotateDg;
+		backgrdRGB.R += (1.0 - 0.6) / (90 * (1.0 / sunRotateDg));
+		backgrdRGB.G -= (1.0 - 0.8) / (90 * (1.0 / sunRotateDg));
+		backgrdRGB.B -= (1) / (90 * (1.0 / sunRotateDg));
+	}
+	else {
+		sunRotate -= 0.00;
+	}
 	glutPostRedisplay();
 }
 
